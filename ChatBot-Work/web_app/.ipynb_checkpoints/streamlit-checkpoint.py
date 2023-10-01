59606adae07e271af415d521c8ab2b75207b247e
Muhammad Ali <muhammadali.atwork@gmail.com>
2023-10-01 14:30:44 +0000

    worked

--- ChatBot-Work/web_app/.ipynb_checkpoints/streamlit-checkpoint.py
+++ ChatBot-Work/web_app/.ipynb_checkpoints/streamlit-checkpoint.py
@@ -4,6 +4,11 @@ import streamlit_authenticator as stauth
 from yaml.loader import SafeLoader
 from streamlit_authenticator import Authenticate
 import streamlit as st
+import openai
+from langchain.document_loaders import NotionDirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
 
 # creds and variables
 with open('authentication_config.yaml') as file:
@@ -16,6 +21,8 @@ authenticator = Authenticate(
     config['preauthorized']
 )
 name, authentication_status, username = authenticator.login('Login', 'main')
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
 
 
 
@@ -25,6 +32,8 @@ if authentication_status:
     authenticator.logout('Logout', 'main')
     st.write(f'Welcome *{name}*')
     st.title('Some content')
+    code_block = "python\nimport DaVinciResolveScript as bmd\n\n# Connect to the DaVinci Resolve project manager\nresolve = bmd.scriptapp('Resolve')\npm = resolve.GetProjectManager()\n\n# Create a new project\nproject = pm.CreateProject('My Project', '/path/to/project')\n\n# Set project settings\nsettings = project.GetSetting()\nsettings['timelineResolutionWidth'] = 1920\nsettings['timelineResolutionHeight'] = 1080\n\n# Save the project\nproject.Save()\n\n# Close the project manager\npm.CloseProjectManager()\n"
+    st.code(code_block)
 elif authentication_status == False:
     st.error('Username/password is incorrect')
 elif authentication_status == None:
