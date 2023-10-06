c30032e7820eb6f44b7b82c48267df5bba7af812
Muhammad Ali <muhammadali.atwork@gmail.com>
2023-10-06 15:06:07 +0000

    syncing

--- ChatBot-Work/web_app/.ipynb_checkpoints/streamlit-checkpoint.py
+++ ChatBot-Work/web_app/.ipynb_checkpoints/streamlit-checkpoint.py
@@ -10,15 +10,18 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 import sys
+from datetime import datetime
+from pathlib import Path
 sys.path.append('/home/ubuntu/workspace/Creds/')
 from openai_config import OPENAI_API_KEY
+import os
 
 # creds and variables
 
 
 # Custom image for the app icon and the assistant's avatar
 company_logo = '/home/ubuntu/workspace/mrwhisper-codespace/ChatBot-Work/web_app/assets/SCR-20231002-owdl.png'
-
+dest_folder = Path('/home/ubuntu/workspace/Temp/UPLOADED-DIR')
 # Configure Streamlit page
 st.set_page_config(
     page_title="MisterWhisper Chatbot",
@@ -53,8 +56,29 @@ if authentication_status:
     #st.write(f'Welcome *{name}*')
     st.title('MisterWhisper ChatBot')
 
+    vector_store = load_vectorstore()
     chain = load_chain()
+    with st.sidebar:
+        st.subheader('you can load your documents here:')
+        with st.form("Upload Documents here", clear_on_submit=True):
+            uploaded_files = st.file_uploader('Upload your documents', type = ['.txt','.pdf'], accept_multiple_files = True)
+            button = st.form_submit_button('Submit!')
+
+        if uploaded_files:
+            if button:
+                time_now = datetime.now().strftime('%Y%m%d-%H%M%S')
+                folder_name = 'Documents-' + time_now 
+                dir_path = dest_folder/folder_name
+                dir_path.mkdir(exist_ok=True, parents=True)
+                for cnt,uploaded_file in enumerate(uploaded_files):
+
+
+                    with open(os.path.join(str(dir_path),uploaded_file.name),"wb") as f:
     
+                        f.write(uploaded_file.getbuffer())         
+                    st.success("Saved File")
+                docs = load_docs_from_files (dir_path)
+                vector_store.add_documents(docs)
     
     for message in st.session_state.messages:
         with st.chat_message(message['role']):
@@ -64,21 +88,27 @@ if authentication_status:
         st.session_state.messages.append({"role": "user", "content": prompt})
         with st.chat_message("user"):
             st.markdown(prompt)
-    #result = chain({"question": prompt, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
-    print(prompt)
-    result = chain({"question": prompt, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
-    #result = chain("Give me code for how to load text documents")
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = result["answer"]
-        message_placeholder.markdown(full_response + "|")
-    message_placeholder.markdown(full_response)
-    
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+        #result = chain({"question": prompt, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
+        print(prompt)
+        #result = chain({"question": prompt, "chat_history": [(message["role"], message["content"]) for message in st.session_state.messages]})
+        #result = chain("Give me code for how to load text documents")
+        
+        result = get_answer(user_input = prompt,_vector_store = vector_store, _chain = chain)
+        #with st.chat_message("assistant"):
+        #    message_placeholder = st.empty()
+        #    full_response = result["answer"]
+        #    message_placeholder.markdown(full_response + "|")
+        #message_placeholder.markdown(full_response)
+        
+        st.session_state.messages.append({"role": "assistant", "content": result})
+        with st.chat_message('assistant'):
+            st.markdown(result)
 
     authenticator.logout('Logout', 'main')
     
 elif authentication_status == False:
     st.error('Username/password is incorrect')
 elif authentication_status == None:
+    st.title('MisterWhisper ChatBot')
+    st.subheader('SignIn for start using it')
     st.warning('Please enter your username and password')
--- ChatBot-Work/web_app/help.py
+++ ChatBot-Work/web_app/help.py
@@ -8,18 +8,71 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import SystemMessagePromptTemplate
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import *
 
 from pathlib import Path
 import sys
+import os
 sys.path.append('/home/ubuntu/workspace/Creds/')
 from openai_config import OPENAI_API_KEY
 openai.api_key = OPENAI_API_KEY
 
 
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=["#","##", "###", "\\n\\n","\\n",".", '\n'],
+    chunk_size=1500,
+    chunk_overlap=100)
+
+
+FILE_LOADER_MAPPING = {
+    ".csv": (CSVLoader, {"encoding": "utf-8"}),
+    ".doc": (UnstructuredWordDocumentLoader, {}),
+    ".docx": (UnstructuredWordDocumentLoader, {}),
+    ".epub": (UnstructuredEPubLoader, {}),
+    ".html": (UnstructuredHTMLLoader, {}),
+    ".md": (UnstructuredMarkdownLoader, {}),
+    ".odt": (UnstructuredODTLoader, {}),
+    ".pdf": (PyPDFLoader, {}),
+    ".ppt": (UnstructuredPowerPointLoader, {}),
+    ".pptx": (UnstructuredPowerPointLoader, {}),
+    ".txt": (TextLoader, {"encoding": "utf8"}),
+    ".ipynb": (NotebookLoader, {}),
+    ".py": (PythonLoader, {}),
+ 
+}
+
+def load_docs_from_files(folder_path):
+    all_files = os.listdir(folder_path)
+    all_files = [i for i in all_files if not i.endswith('.ipynb_checkpoints')]
+    print(all_files)
+    loaded_documents = []
+    for filename in all_files:
+        #try:
+            full_pth = Path(f'{folder_path}/{filename}')
+            ext = os.path.splitext(full_pth.name)[-1][1:].lower()
+            if ext in FILE_LOADER_MAPPING:
+                loader_class, loader_args = FILE_LOADER_MAPPING[ext]
+                loader = loader_class(str(full_pth), **loader_args)
+            else:
+                loader = UnstructuredFileLoader(str(full_pth))
+        #except:
+        #    pass
+        #try:
+            loaded_documents.extend(loader.load())
+            splitted_docs = text_splitter.split_documents(loaded_documents)
+        #except:
+        #    pass
+    return splitted_docs
+
+
 
 template = """
 You are a customized Chatbot, and will be helping in different domains; like code helping, omniverse stuff, ShotGrid docs, api stuff etc. 
-It is very important that you do have memory, and can go along with the User, and also make use of Knowledge base, which will be provided to you with some workflow.
+You will have memory, and previous messages will be provided to you in following format: \n
+### Input  <Human Message>
+### Response <Your Reply> 
+and also make use of Knowledge base, which will be provided to you with some workflow.
 -- If any code is to be written, DO WRITE IT INSIDE ``` <CODE> ``` . This is very important, as this way, the UI will be able to display code in well formatted 
 way, by using some post-processing.
 
@@ -28,19 +81,15 @@ way, by using some post-processing.
 this is the running chat-history:
 {chat_history}
 
-Query:  {question}
+### Input:  {question}
 
-Response:
+### Response:
 """
 prompt = PromptTemplate(
     input_variables = ['context', 'question', 'chat_history'], template = template
 )
 
-memory = ConversationBufferWindowMemory(
-    k = 4,
-    memory_key = 'chat_history',
-    return_messages = False
-)
+
 
 llm = ChatOpenAI(model = 'gpt-3.5-turbo-16k',
                 openai_api_key= OPENAI_API_KEY)
@@ -48,10 +97,21 @@ llm = ChatOpenAI(model = 'gpt-3.5-turbo-16k',
 embeddings = OpenAIEmbeddings(model = 'text-embedding-ada-002',
                               openai_api_key= OPENAI_API_KEY)
 
-text_splitter = RecursiveCharacterTextSplitter(
-    separators=["#","##", "###", "\\n\\n","\\n",".", '\n'],
-    chunk_size=1500,
-    chunk_overlap=100)
+
+@st.cache_resource(show_spinner=False)
+def load_chain():
+    memory = ConversationBufferWindowMemory(
+        k = 4,
+        memory_key= 'chat_history',
+        human_prefix = '### Input',
+        ai_prefix = '### Response',
+        input_key = 'question',
+        output_key = 'output_text',
+        return_messages = True
+    )
+    chain = load_qa_chain(llm,chain_type='stuff',prompt = prompt, memory = memory, verbose =True)
+    return chain
+
 
 
 @st.cache_resource(show_spinner=False)
@@ -66,18 +126,17 @@ def load_vectorstore():
 
 
 vector_store = load_vectorstore()
+chain = load_chain()
 @st.cache_data
-def load_chain(vector_store = vector_store, llm = llm, memory = memory):
+def get_answer(user_input,_vector_store = vector_store, _chain = chain):
 
-    retriever = vector_store.as_retriever(search_kwargs={"k": 3})
-    chain = ConversationalRetrievalChain.from_llm(llm=llm, 
-                                              retriever=retriever, 
-                                              memory=memory, 
-                                              #get_chat_history=lambda h : h,
-                                              combine_docs_chain_kwargs = {'prompt':prompt},
-                                              verbose=True)
+    
+    docs = vector_store.similarity_search(user_input)
+    answer = chain.run({'input_documents':docs,'question':user_input})
 
-    return chain
+
+
+    return answer
 
 
     
