412edaae289a57078e50185acfa03964e5318d77
Muhammad Ali <muhammadali.atwork@gmail.com>
2023-10-07 20:13:46 +0000

    given

---
+++ ChatBot-Work/trial_work/.ipynb_checkpoints/plain_pinecone_help-checkpoint.py
@@ -0,0 +1,227 @@
+import openai
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+import json
+import os
+from pathlib import Path
+import sys
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.document_loaders import *
+from datetime import datetime
+from langchain.vectorstores import Pinecone
+
+
+import sys
+sys.path.append('/home/ubuntu/workspace/Creds')
+from openai_config import OPENAI_API_KEY, MW_ENVIRONMENT, MW_PINECONE_API_KEY
+openai.api_key = OPENAI_API_KEY
+
+embeddings = OpenAIEmbeddings(model = 'text-embedding-ada-002',
+                              openai_api_key= OPENAI_API_KEY)
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=["#","##", "###", "\\n\\n","\\n",".", '\n'],
+    chunk_size=1500,
+    chunk_overlap=100)
+
+def running_summarize(chat_history, new_query):
+
+
+    user_message =    f""" Current Summary:
+    {chat_history}
+    
+    New lines of conversation :
+    {new_query}
+    
+    New_Summary:
+
+    """
+    message = [
+            {'role':'system','content':'''
+            You are acting as dynamic memory of a chatbot(Chatbot: A customized one, for helping with latest python libraries, tools like omniverse, usd files etc). You have to be
+            mindful, that in dynamic summary generation, you have to return the corrected code/ any configuration related stuff etc. As, there will be a button in USEr Interface, with which 
+            your running summary(at that point) will be saved to a vectorstore. So the purpose is, for Example, user is chatting about a recent python library code. and now over few iterations
+            of conversations, you both were able to get the working code/config. Now the user wants to save this in vectorstore, so later anyone asks somethingspecifc/related to that, 
+            because of your SHORT-EFFECTIVE summary saved.  So you will have Current
+            -summary, new lines of conversations, and you have to generate new Summary. 
+            ******** DO INCLUDE IN PROVIDED/CORRECTED/REQUIRED CODE OR CONFIG - AND BE LESS VERBOSE, FOCUS ON IMPORTANT THINGS ONLY *********:: 
+        
+            '''},
+            
+            {'role':'user','content':user_message}
+            
+            
+        ]
+
+    
+    response = openai.ChatCompletion.create(
+                            #model="gpt-3.5-turbo-16k",
+                            #model="gpt-3.5-turbo",
+                            model = 'gpt-4',
+                            messages=message,
+                            temperature=0.7
+                                    )
+    return response['choices'][0].message.content
+
+
+
+def return_docs_from_pinecone_lang( query,pinecone_index, embeddings =embeddings):
+
+    '''
+    will return top 3 relevant chunks, and also top one, for decision making
+    '''
+    
+    docs = pinecone_index.similarity_search(query, k =3)
+
+    final_str = ''
+    for one in docs:
+        final_str += one.page_content
+        final_str +='\n\n\n\n'
+
+    return final_str, docs[0].page_content
+
+
+def main_chat_fn(query, vstore, chat_summary):
+
+    context,decision_context = return_docs_from_pinecone_lang(query,vstore)
+    to_use_context = decision_to_use_context(decision_context,query)
+    to_use_context_decision = get_response_type(to_use_context)
+    print(to_use_context)
+    if to_use_context_decision == 'Yes':
+        context = context
+        print('USED:::')
+    elif to_use_context_decision =='No':
+        context = '''We don't have any relevant Information, so use your best knowledge, and if you don't know, say : i don't know. Kindly provide me some context, or internet access'''
+        print(context)
+
+    user_message = f"""
+    Context of knowledgebase:
+    -----
+    {context}
+    -----
+    Running summary of chat:
+    -----
+    {chat_summary}
+    ----
+    Query : {query}
+
+    AI Response:
+
+    """
+    message = [
+        {'role':'system','content':'''You are a customized Chatbot, and will be helping in different domains; like code helping, omniverse stuff, ShotGrid docs, api stuff etc. 
+You will have memory, and previous messages will be provided to you. Also, custom knowlwdgebase, similar chunks of info, will be provided as context.'''},
+        {'role':'user','content':user_message}
+        
+        
+    ]
+
+    
+    response = openai.ChatCompletion.create(
+                            #model="gpt-3.5-turbo-16k",
+                            model = 'gpt-4',
+                            messages=message,
+                            temperature=0.7
+                                    )
+    return response['choices'][0].message.content
+
+
+def decision_to_use_context(docs,query):
+
+    messages = [
+        {'role':'system','content':"""
+        We have large store of knowledgebase texts, and we have one api/fn that based on QUERY returns relevant chunks of texts. Now, it is error-prone, 
+        like even if relevant info is not present in knowledgebase, it does return something.
+        
+        **Be mindful in cases of code, It should be returning docs of relevant library, 
+        and methods, not of any random one. !! IF ERROR IS IN FIFTYONE DATASETS METHOD/OBJECT LIBRARY RELATED, CONTEXTS SHOULD NOT BE SOME CODE-RELATED TO ERROR/SOLVING ABOUT LANGCHAIN DATASETS/ETC,SHOTGRID ETC
+        You have to make the decision , Whether than information is good to use or not.**
+        
+        You have to only Answer in "Yes:Small Reason" and "No: Small Reason". There is no option of Not knowing, or any explanation stuff.
+         \n\n
+                                """},
+        {'role':'user','content':"""
+        Query is : "got this error
+        AttributeError Traceback (most recent call last) Cell In[8], line 1 ----> 1 classes = dst.classifications.classes 2 print(classes)
+        
+        File ~/.pyenv/versions/env-yolov8/lib/python3.10/site-packages/fiftyone/core/dataset.py:357, in Dataset.getattribute(self, name) 354 if getattr(self, "_deleted", False): 355 raise ValueError("Dataset '%s' is deleted" % self.name) --> 357 return super().getattribute(name)
+        
+        AttributeError: 'Dataset' object has no attribute 'classifications'", \n
+        Context docs are : "# we drop sparse_values as they are not needed for this example\n\ndataset.documents.drop([\n\n\'metadata\'], axis\n\n1, inplace\n\nTrue)\n\ndataset.documents.rename(columns\n\n={\n\n\'blob\':\n\n\'metadata\'}, inplace\n\nTrue)\n\ndataset.head()\n\n0 \n       417ede5d-39be-498f-b518-f47ed4e53b90 \n       [0.005949743557721376, 0.01983247883617878, -0... \n       {\'chunk\': 0, \'text\': \'.rst\n.pdf\nWelcome to Lan... \n     \n       1 \n       110f550d-110b-4378-b95e-141397fa21bc \n       [0.009401749819517136, 0.02443608082830906, 0..\n\n\n\nGPT4 with\nRetrieval Augmentation over LangChain Docs\n\nIn this notebook we\'ll work through an example of using GPT-4 with\nretrieval augmentation to answer questions about the LangChain Python\nlibrary.\n\nTo begin we must install the prerequisite libraries:\n\n!pip install -qU \\\n\nopenai==0.27.7 \\\n\n"pinecone-client[grpc]"==2.2.1 \\\n\npinecone-datasets==\'0.5.0rc11\'\n\n🚨 Note: the above pip install is formatted for\nJupyter notebooks. If running elsewhere you may need to drop the\n!n\n\n\n"
+        """},
+        {'role':'assistant','content': "No: As query is for Fiftyone Related, and context is about Langchain, GPT4,etc"},
+
+
+        {'role':'user','content':f"""
+        Query is : "{query}" \n
+        Context docs are : "{docs}"
+        """}
+    ]
+    response = openai.ChatCompletion.create(
+                            #model="gpt-3.5-turbo-16k",
+                            model = 'gpt-4',
+                            messages=messages,
+                            temperature=0
+                                    )
+    return response['choices'][0].message.content
+
+
+def update_pinecone_texts(pinecone_index, texts_to_add, index_name):
+    
+    #index_name = pinecone_index.configuration.server_variables['index_name']
+    updated_index = Pinecone.from_texts(texts_to_add,embeddings, index_name = index_name)
+    return updated_index
+def update_pinecone_docs(pinecone_index, docs_to_add, index_name):
+
+    #index_name = pinecone_index.configuration.server_variables['index_name']
+    updated_index = Pinecone.from_documents(docs_to_add,embeddings, index_name = index_name)
+    return updated_index
+
+
+def document_text_extraction(extension,file_path):
+
+    
+    if extension == "json":
+        loader = ChatGPTLoader(log_file=str(file_path), num_logs=0)
+        docs = loader.load()
+    elif extension == "txt":
+        loader = TextLoader(str(file_path))
+        docs = loader.load()
+    elif extension =='pdf':
+        loader = PyPDFLoader(str(file_path))
+        docs = loader.load_and_split()
+    elif extension == 'docx':
+        loader = Docx2txtLoader(str(file_path))
+        docs = loader.load()
+    
+    docs = text_splitter.split_documents(docs)
+    print(len(docs))
+
+
+    return docs
+
+
+def get_last_two_conversations(messages):
+    # Ensure there are at least 4 messages
+    if len(messages) < 4:
+        # Return whatever messages are available
+        last_messages = messages
+    else:
+        # Get the last 4 messages (2 conversations)
+        last_messages = messages[-4:]
+    
+    result = ""
+    for message in last_messages:
+        if message["role"] == "user":
+            result += f"User : {message['content']}\n"
+        elif message["role"] == "assistant":
+            result += f"AI ASSISTANT : {message['content']}\n\n"
+    
+    return result
+
+
+
+def get_response_type(s):
+    return s.split(":")[0].strip()
+
+
+
---
+++ ChatBot-Work/trial_work/plain_pinecone_str.py
@@ -0,0 +1,193 @@
+import streamlit as st
+import os
+import shutil
+from pathlib import Path
+import openai
+from plain_pinecone_help import *
+from langchain.document_loaders.chatgpt import ChatGPTLoader
+from langchain.vectorstores import Pinecone
+import pinecone
+from openai_config import OPENAI_API_KEY, MW_ENVIRONMENT, MW_PINECONE_API_KEY
+
+
+
+dest_folder = Path('/home/ubuntu/workspace/Temp/UPLOADED-DIR')
+
+
+# defining the session_states
+if 'vectorstore_require_update' not in st.session_state:
+    st.session_state.vectorstore_require_update = False
+
+if 'vectorstore_loaded' not in st.session_state:
+    st.session_state.vectorstore_loaded = False
+
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = ''
+
+if 'running_chat_summary' not in st.session_state:
+    st.session_state.running_chat_summary = ''
+
+
+st.title('MrWhisperTrial-ChatBot')
+#vstore_path = '/home/ubuntu/workspace/Temp/07-14Oct/minimal_knowledgebase'
+pinecone.init(
+    api_key= MW_PINECONE_API_KEY,  # find at app.pinecone.io
+    environment= MW_ENVIRONMENT,  # next to api key in console
+)
+pinecone_mw_index_name = 'minimal-index-dynamic-chatbot'
+
+pinecone.init(
+    api_key= MW_PINECONE_API_KEY,  # find at app.pinecone.io
+    environment= MW_ENVIRONMENT,  # next to api key in console
+)
+
+
+#pinecone_index = Pinecone.get_pinecone_index(pinecone_mw_index_name)   This is raw pinecone index
+pinecone_index = Pinecone.from_existing_index(pinecone_mw_index_name,embeddings)
+     
+if st.button('Start new Chat'):
+    st.session_state.messages = []
+    st.session_state.chat_history = ''
+    st.session_state.running_chat_summary = ''
+prompt = st.chat_input('Ask Anything?')
+
+
+if len(st.session_state.messages) > 1:
+    for message in st.session_state.messages:
+        with st.chat_message(message['role']):
+            st.markdown(message['content'])
+
+#print(f':::::::::::::;{st.session_state.running_chat_summary}')
+
+        
+
+if prompt: 
+
+    st.session_state.messages.append({'role':'user','content':prompt})
+    with st.chat_message('user'):
+        st.markdown(prompt)
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        
+
+        
+    if len(st.session_state.messages) > 1:
+        chat_history = ''
+        for message in st.session_state.messages:
+            if message['role'] == 'user':
+                chat_history += f"User :: {message['content']} \n"
+            elif message['role'] == 'assistant':
+                chat_history += f"AI Reponse :: {message['content']} \n"
+        # here with summary, only last 2 full conversations should go
+        last_two_conversations = get_last_two_conversations(st.session_state.messages)
+        chat_summary = running_summarize(st.session_state.running_chat_summary, last_two_conversations)
+        st.session_state.running_chat_summary = chat_summary
+    else:
+        chat_summary = ''
+    print(f"CHAT-SUMMARY::::::::::::\n")
+    #print(chat_summary)
+    answer = main_chat_fn(prompt,pinecone_index,chat_summary)
+    with open('/home/ubuntu/workspace/Temp/07-14Oct/testing_summaries.txt','a') as f:
+        f.write(chat_summary)
+        f.write('\n\n\n ----------------------------------- \n\n\n')
+    st.session_state.messages.append({"role": "assistant", "content": answer})
+
+    with st.chat_message('assistant'):
+        st.markdown(answer)
+
+
+with st.sidebar:
+
+    with st.form("Upload ChatGpt JSON export here", clear_on_submit=True):
+        extension = 'json'
+        uploaded_files = st.file_uploader('Upload your ChatGpt JSON export here', type = ['.json',], accept_multiple_files = True)
+        button = st.form_submit_button('Submit!')
+        if uploaded_files:
+            if button:
+                time_now = datetime.now().strftime('%Y%m%d-%H%M%S')
+                folder_name = f'{extension.upper()}-Documents-{time_now}'
+                dir_path = dest_folder/folder_name
+                dir_path.mkdir(exist_ok=True, parents=True)
+                for uploaded_file in uploaded_files:
+                    #bytes_data = uploaded_file.read()
+                    with open(os.path.join(str(dir_path), uploaded_file.name), "wb") as f:
+                        f.write(uploaded_file.getbuffer())         
+                    st.success("Saved File")
+                try:
+                    docs = document_text_extraction('json',str(dir_path/uploaded_file.name))                
+                    load_vstore = update_pinecone_docs(pinecone_index,docs,pinecone_mw_index_name)
+                except Exception as e:
+                    print(e)
+                    st.write('Sorry we are not able to add your document to vectorstore')
+                    pass
+                
+    with st.form("Upload Google Docx here", clear_on_submit=True):
+        uploaded_files = st.file_uploader('Upload Google Docx', type = ['.docx'], accept_multiple_files = True)
+        button = st.form_submit_button('Submit!')
+        if uploaded_files:
+            if button:
+                time_now = datetime.now().strftime('%Y%m%d-%H%M%S')
+                folder_name = f'{extension.upper()}-Documents-{time_now}'
+                dir_path = dest_folder/folder_name
+                dir_path.mkdir(exist_ok=True, parents=True)
+                for uploaded_file in uploaded_files:
+                    #bytes_data = uploaded_file.read()
+                    with open(os.path.join(str(dir_path), uploaded_file.name), "wb") as f:
+                        f.write(uploaded_file.getbuffer())         
+                    st.success("Saved File")
+                try:   
+                    docs = document_text_extraction('docx',str(dir_path/uploaded_file.name))
+                    load_vstore = update_pinecone_docs(pinecone_index,docs,pinecone_mw_index_name)
+                except Exception as e:
+                    print(e)
+                    st.write('Sorry we are not able to add your document to vectorstore')
+                    pass
+
+                
+                load_vstore = update_vectorstore_docs(load_vstore,docs)
+    with st.form("Upload PDF here", clear_on_submit=True):
+        uploaded_files = st.file_uploader('Upload pdf', type = ['.pdf'], accept_multiple_files = True)
+        button = st.form_submit_button('Submit!')
+        if uploaded_files:
+            if button:
+
+                time_now = datetime.now().strftime('%Y%m%d-%H%M%S')
+                folder_name = f'{extension.upper()}-Documents-{time_now}'
+                dir_path = dest_folder/folder_name
+                dir_path.mkdir(exist_ok=True, parents=True)
+                for uploaded_file in uploaded_files:
+                    #bytes_data = uploaded_file.read()
+                    with open(os.path.join(str(dir_path), uploaded_file.name), "wb") as f:
+                        f.write(uploaded_file.getbuffer())         
+                    st.success("Saved File")
+                try:
+                    docs = document_text_extraction('pdf',str(dir_path/uploaded_file.name))
+                    
+                    load_vstore = update_pinecone_docs(pinecone_index,docs,pinecone_mw_index_name)
+                except Exception as e:
+                    print(e)
+                    st.write('Sorry we are not able to add your document to vectorstore')
+                    pass
+
+
+
+
+    
+    if len(st.session_state.messages)>2:
+        st.title('Running Summary')
+        st.markdown(st.session_state.running_chat_summary)
+        if st.button('Add to VectorStore?'):
+            document_to_add = st.session_state.running_chat_summary
+            print('-----')
+            print(type(document_to_add), document_to_add)
+            docs  = text_splitter.split_text(document_to_add)
+            print(len(docs))
+            load_vstore = update_pinecone_texts(pinecone_index,docs,pinecone_mw_index_name)
+
+
+
+
+
+
